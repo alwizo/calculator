1700886c09021ecf1a4dc4e915fa8e4183b5d65d
alwizo <alwizo.com@gmail.com>
2017-08-07 17:21:17 +0200

    Now we can type digits to a display

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var display: UILabel!
+    var userIsInAMiddleOfTyping = false
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
+        if(userIsInAMiddleOfTyping){
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        }else{
+            display!.text = digit
+            userIsInAMiddleOfTyping = true
+        }
         print("\(digit) was called")
     }
     
-
+    
 }
 
