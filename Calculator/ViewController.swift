22edba5499a0e1869129aeebd2ac981e2dbb7929
alwizo <alwizo.com@gmail.com>
2017-08-07 16:05:24 +0200

    Added touchDigit func

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        print("\(digit) was called")
     }
     
 
